8866f2472a38f5a1fdf5692e9997c38f3625ab81
Jose <jose-miguel.felix-de-los-rios@etu.umontpellier.fr>
2024-10-16 20:34:23 +0200

    Vérification d'existance d'un fichier

--- main.sh
+++ main.sh
@@ -1,9 +1,15 @@
-#tester si le fichier existe 
-
-echo $@
+#tester si le fichier existe
+if [ -z $@ ]
+then
+    echo "Aucun fichier specifié"
+elif [ -f $@ ]
+then
+     echo "Le fichier existe"
+else
+    echo "Le fichier specifié n'existe pas ou n'est pas un fichier"
+fi
 
 #importer le fichier avec python
-
 
 
 #stocker la colonne i 
