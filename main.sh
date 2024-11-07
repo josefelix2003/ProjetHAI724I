94ddadc09a17d601c0f81b2ada5e444f0c64e6d2
Jose <jose-miguel.felix-de-los-rios@etu.umontpellier.fr>
2024-11-07 15:40:08 +0100

    Fichier main et intensite

--- main.sh
+++ main.sh
@@ -1,15 +1,26 @@
+#Le parametre 1 correspond au nom du fichier, parametre 2 correspond a la taille 
+fichier=$1
+taille=$2
+
 #tester si le fichier existe
-if [ -z $@ ]
+if [ -z $1 ]
 then
     echo "Aucun fichier specifié"
-elif [ -f $@ ]
+    exit 1
+elif [ -f $1 ]
 then
      echo "Le fichier existe"
 else
     echo "Le fichier specifié n'existe pas ou n'est pas un fichier"
+    exit 1
 fi
 
-#importer le fichier avec python
+echo "Etape suivante"
+
+#importer le fichier avec le script python intensite.py
+
+python3 intensite.py $fichier $taille
+
 
 
 #stocker la colonne i 
