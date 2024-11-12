b7838b94ca82bb8cbe9e142fe822519f24da92d0
Jose <jose-miguel.felix-de-los-rios@etu.umontpellier.fr>
2024-11-12 10:42:43 +0100

    Ajout fonction intensite()

--- intensite.py
+++ intensite.py
@@ -16,81 +16,83 @@
 #------------------------------------------------------------------------
 
 import re
-
-fichier = open("Spectre_photoluminescence.txt")
-taille = 10
-
-
-lignecherche = False
-debut = False
-
-longueur_in=0
-longueur_fi=0
-
-longueurs_onde = []
-intensites = []
-
-for lignecourant in fichier:
-    res = re.search("Begin", lignecourant)
-    if res :
-        lignecherche = True
-        continue
-        
-    if lignecherche == True :
-        res = re.search("End", lignecourant)
-        if res:
-            break
-        else :
-            #ligne courant correspond à une ligne de données
-            l=lignecourant.strip().split()
+def intensite():
+    fichier = open("Spectre_photoluminescence.txt")
+    taille = 10
+    
+    
+    lignecherche = False
+    debut = False
+    
+    longueur_in=0
+    longueur_fi=0
+    
+    longueurs_onde = []
+    intensites = []
+    
+    for lignecourant in fichier:
+        res = re.search("Begin", lignecourant)
+        if res :
+            lignecherche = True
+            continue
             
-            if debut == False:
-                longueur_in = float(l[0])
-                debut = True
+        if lignecherche == True :
+            res = re.search("End", lignecourant)
+            if res:
+                break
+            else :
+                #ligne courant correspond à une ligne de données
+                l=lignecourant.strip().split()
                 
-            longueur_fi = float(l[0]) 
-            
-            longueurs_onde.append(float(l[0]))
-            intensites.append(float(l[1]))
-            
+                if debut == False:
+                    longueur_in = float(l[0])
+                    debut = True
+                    
+                longueur_fi = float(l[0]) 
                 
- 
-
-fenetres = []
-longueur_a = int(longueur_in)
-
+                longueurs_onde.append(float(l[0]))
+                intensites.append(float(l[1]))
+                
+                    
+     
     
-while longueur_a < longueur_fi:
-    fenetres.append((longueur_a,longueur_a+taille))
-    longueur_a+=taille
+    fenetres = []
+    longueur_a = int(longueur_in)
     
-nb_fenetres = len(fenetres)
-
-valeurs_dict = []
-
-for n in range(nb_fenetres):
-    valeurs_dict.append([])
-
-
-
-for n in range(nb_fenetres):
-    a=fenetres[n][0] #debut fenetre
-    b=fenetres[n][1] #fin fenetre
-    
-    for p in range(len(longueurs_onde)):
-        l=longueurs_onde[p]
-        i=intensites[p] #intensite associé a la longueur d'onde l
-        if l>=a and l<b :
-            valeurs_dict[n].append(i) #on ajoute cette intensité à la liste d'indice n 
         
-
-data = {}
-for n in range(nb_fenetres):
-    data[fenetres[n]]=valeurs_dict[n]
-print(data)
-
-fichier.close()
+    while longueur_a < longueur_fi:
+        fenetres.append((longueur_a,longueur_a+taille))
+        longueur_a+=taille
+        
+    nb_fenetres = len(fenetres)
     
+    valeurs_dict = []
+    
+    for n in range(nb_fenetres):
+        valeurs_dict.append([])
+    
+    
+    
+    for n in range(nb_fenetres):
+        a=fenetres[n][0] #debut fenetre
+        b=fenetres[n][1] #fin fenetre
+        
+        for p in range(len(longueurs_onde)):
+            l=longueurs_onde[p]
+            i=intensites[p] #intensite associé a la longueur d'onde l
+            if l>=a and l<b :
+                valeurs_dict[n].append(i) #on ajoute cette intensité à la liste d'indice n 
+            
+    
+    data = {}
+    for n in range(nb_fenetres):
+        data[fenetres[n]]=valeurs_dict[n]
+    
+    fichier.close()
+    
+    return data
+
+
 
     
     
