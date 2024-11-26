0d11b6c9eea9191956527e0553b3f11354ddd5c6
josefelix2003 <jose-miguel.felix-de-los-rios@etu.umontpellier.fr>
2024-11-26 08:36:22 +0100

    Modifications Alban
    Ajout de vérification de la validité de la taille de fenêtre

--- main.sh
+++ main.sh
@@ -5,16 +5,33 @@ echo "Veuillez saisir la taille de la fenêtre"
 read taille
 
 
-#tester si le fichier existe
-if [ -z $fichier ]
+#tester la validité du fichier
+if [ -z $fichier ] # vérifier si une chaîne a été fourni
 then
     echo "Aucun fichier specifié"
     exit 1
-elif [ -f $fichier ]
+elif [ -f $fichier ]#tester si le fichier existe dans le dossier courant et que c'est bien un fichier
 then
      echo "Le fichier existe"
+     echo $fichier
 else
     echo "Le fichier specifié n'existe pas ou n'est pas un fichier"
+    exit 1
+fi
+
+#tester la validité de la fênetre
+
+if [ -z $taille ]
+then
+    echo "Aucune valeur a été saisie; La valeur par défaut sera 10 nm"
+    taille=10
+    echo $taille
+elif [ $taille =~ ^[0-9]+$ ]
+then 
+    echo "La taille saisie est bien un entier positif : " 
+    echo $taille
+else
+    echo "La taille saisie n'est pas valide"
     exit 1
 fi
 
