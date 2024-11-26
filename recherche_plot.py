c3a642c4208dcd09c4587ced96a70950ac14ff33
albandesoutter <alban.desoutter@etu.umontpellier.fr>
2024-11-26 09:33:08 +0100

    Update recherche_plot.py

--- recherche_plot.py
+++ recherche_plot.py
@@ -1,1 +1,19 @@
+Un deuxième script recherche_plot.py aura pour but de lister les intensités pour un intervalle de
+longueur d'onde donnée demandée par l'utilisateur (fonction input) en utilisant l'index et d'afficher
+graphiquement les intensités en fonction des longueurs d'onde pour l'intervalle demandé.
+
+
+prenom = input('Entrez votre prénom (entre guillemets) : ')
+
+float(intervalledebut)=input('veuillez indiquer un début d'intervalle pour les longueurs d'onde pour lesquelles vous souhaitez avoir les intensités correspondantes') 
+float(intervallefin)=input('veuillez indiquer une fin d'intervalle pour les longueurs d'ondes pour lesquelles vous souhaitez avoir les intensités correspondantes')
+
+print 'voici les intensités correspondantes à cet intervalle' 
+
+
+
+if longueurs_onde > intervalledebut
+and if longueurs_ondes < intervallefin
+
+intensite
 
