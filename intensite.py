0222f5818757e13c6f33cfe834e32eabc0872c15
Jose <jose-miguel.felix-de-los-rios@etu.umontpellier.fr>
2024-12-03 08:34:31 +0100

    modification instensite.py

--- intensite.py
+++ intensite.py
@@ -1,94 +1,74 @@
 
-
-#------------------------------------------------------------------------
-#A decommenter :
+import re
+import sys
     
 #stocker paramètres d'entrée    
-#import sys
+nom_fichier = sys.argv[1] #nom du fichier contenant les données
+taille =  int(sys.argv[2]) # taille de chaque fenetre
 
-#fichier = sys.argv[1]
 
-#if len(sys.argv) == 3:
-#    taille = int(sys.argv[2])
-#else:
-#    taille = 10
-
-#------------------------------------------------------------------------
-
-import re
-def intensite():
-    fichier = open("Spectre_photoluminescence.txt")
-    taille = 10
+def intensite(nom_fichier, taille):
+    fichier = open(nom_fichier) #les données seront stockés dans cet variable
+    #On extrait ces donées pour les stocker sous forme de listes
     
     
-    lignecherche = False
-    debut = False
-    
-    longueur_in=0
-    longueur_fi=0
-    
+    lignecherche = False #boolean indiquant si on a commencé a parcourir les lignes contenant les données; On l'initialise en False car les premieres lignes corresponent a l'information générale des mesures
     longueurs_onde = []
     intensites = []
     
     for lignecourant in fichier:
         res = re.search("Begin", lignecourant)
-        if res :
-            lignecherche = True
-            continue
+        if res : #Si la regexp a été trouvée dans la ligne courante
+            lignecherche = True #on indique qu'on a commence a parcourir les lignes contenant les données
+            continue #On passe directement a la ligne suivante
             
         if lignecherche == True :
-            res = re.search("End", lignecourant)
-            if res:
+            res = re.search("End", lignecourant) #On veut vérifier qu'on est toujours dans la partie données
+            if res: #Dans ce cas on a fini de récuperer les données, on arrete de lire le document
                 break
-            else :
-                #ligne courant correspond à une ligne de données
-                l=lignecourant.strip().split()
+            else : 
+                #la ligne courante correspond a des données qu'on veut extraire
+                l=lignecourant.strip().split() #on transforme la ligne en une liste contenant la longueur d'onde et l'intensité associée 
                 
-                if debut == False:
-                    longueur_in = float(l[0])
-                    debut = True
-                    
-                longueur_fi = float(l[0]) 
-                
-                longueurs_onde.append(float(l[0]))
-                intensites.append(float(l[1]))
+                longueurs_onde.append(float(l[0])) #on ajoute l'élément d'indice 0 aux longueurs d'onde
+                intensites.append(float(l[1])) #on ajoute l'élément d'indice 1 aux intensités
                 
                     
      
     
-    fenetres = []
-    longueur_a = int(longueur_in)
+    fenetres = [] #La liste contenant les intervalles de longueurs d'onde
+    longueur_min = int(min(longueurs_onde))
+    derniere_longueur = int(max(longueurs_onde))
     
         
-    while longueur_a < longueur_fi:
-        fenetres.append((longueur_a,longueur_a+taille))
-        longueur_a+=taille
+    while longueur_min < derniere_longueur: #Tant qu'on n'est pas arrivé a la derniere longueur en crée une fenetre
+        fenetres.append((longueur_min,longueur_min+taille))
+        longueur_min+=taille
         
     nb_fenetres = len(fenetres)
-    
     valeurs_dict = []
     
     for n in range(nb_fenetres):
-        valeurs_dict.append([])
+        valeurs_dict.append([]) #on ajoute une liste vide pour chaque intervalle de longueurs d'onde
     
     
     
     for n in range(nb_fenetres):
-        a=fenetres[n][0] #debut fenetre
-        b=fenetres[n][1] #fin fenetre
+        a=fenetres[n][0] #limite inférieure de la fenetre
+        b=fenetres[n][1] #limite supérieure de la fenetre
         
-        for p in range(len(longueurs_onde)):
-            l=longueurs_onde[p]
-            i=intensites[p] #intensite associé a la longueur d'onde l
-            if l>=a and l<b :
-                valeurs_dict[n].append(i) #on ajoute cette intensité à la liste d'indice n 
+        for l in range(len(longueurs_onde)):
+            longueur=longueurs_onde[l]
+            intensite=intensites[l] #intensite associé a la longueur d'onde l
+            if longueur>=a and longueur<b : #si la longueur est comprise entre la limite inf et sup de la fenetre
+                valeurs_dict[n].append(intensite) #on ajoute cette intensité à la liste d'indice n 
             
     
     data = {}
-    for n in range(nb_fenetres):
-        data[fenetres[n]]=valeurs_dict[n]
+    for n in range(nb_fenetres): #on va parcourir les différentes fenetres
+        data[fenetres[n]]=valeurs_dict[n] #fenetres[n] correspond a la clé du dictionnaire; valeurs_dict[n] définit la valeurs associée a chaque clé
     
-    fichier.close()
+    #fichier.close()
     
     #Print max, min, moyenne pour chaque fenêtre
     for k in data:
@@ -107,9 +87,10 @@ def intensite():
     
     
     
-    return data
+    return data, intensites, longueurs_onde
 
-intensite()
+print(intensite(nom_fichier, taille)[0])
+
 
 
 
