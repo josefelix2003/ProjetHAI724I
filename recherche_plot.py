7e9c548ac80b56a0281c5bfe5e3c780275f7a474
josefelix2003 <jose-miguel.felix-de-los-rios@etu.umontpellier.fr>
2024-12-03 10:17:44 +0100

    Update recherche_plot.py

--- recherche_plot.py
+++ recherche_plot.py
@@ -15,8 +15,8 @@ longueurs_ondesint=[] #initialisation de mon tableau de valeur d'intérêt pour 
 intensitesint=[]
 
 
-intervalledebut=float(input('veuillez indiquer un début d intervalle pour les longueurs d onde pour lesquelles vous souhaitez avoir les intensités correspondantes')) #fonction input pour que l'utilisateur définisse les brones de l'intervalle
-intervallefin=float(input('veuillez indiquer une fin d intervalle pour les longueurs d ondes pour lesquelles vous souhaitez avoir les intensités correspondantes'))
+intervalledebut=float(input('Veuillez indiquer un début d intervalle pour les longueurs d onde pour lesquelles vous souhaitez avoir les intensités correspondantes\n')) #fonction input pour que l'utilisateur définisse les brones de l'intervalle
+intervallefin=float(input('Veuillez indiquer une fin d intervalle pour les longueurs d ondes pour lesquelles vous souhaitez avoir les intensités correspondantes\n'))
 
 print ('voici les intensités correspondantes à cet intervalle et la représentation graphique')
 
