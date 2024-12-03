90bdfe44259391e2340079f581ce0156e25b9388
josefelix2003 <jose-miguel.felix-de-los-rios@etu.umontpellier.fr>
2024-12-03 11:03:21 +0100

    Update recherche_plot.py

--- recherche_plot.py
+++ recherche_plot.py
@@ -33,7 +33,7 @@ print (intensitesint)
 
 
 plt.plot(longueurs_ondesint, intensitesint) #crée le plot, mais ne l'affiche pas
-plt.xlabel('Longueurs d'onde')
-plt.ylabel('Intensite (a.u.)')
-plt.title('Spectre photoluminescence')
+plt.xlabel("Longueurs d'onde")
+plt.ylabel("Intensite (a.u.)")
+plt.title("Spectre photoluminescence")
 plt.show() #affiche le plot
