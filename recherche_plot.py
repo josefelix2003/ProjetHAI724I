0444eb16801a6a84722f636ac7df3b54374482f0
albandesoutter <alban.desoutter@etu.umontpellier.fr>
2024-12-03 09:10:26 +0100

    Update recherche_plot.py

--- recherche_plot.py
+++ recherche_plot.py
@@ -4,15 +4,15 @@ from intensite import intensite
 
 
 intensite=intensite(nomfichier, taille)[1]
-longueur_ondes=intensite(nomfichier, taille)[2] #voir siplifier un seul appel
+longueurs_ondes=intensite(nomfichier, taille)[2] #voir siplifier un seul appel
 
 
 longueurs_ondesint=[] #initialisation de mon tableau de valeur d'intérêt pour lambda et I
 intensitesint=[]
 
 
-float(intervalledebut)=input('veuillez indiquer un début d intervalle pour les longueurs d onde pour lesquelles vous souhaitez avoir les intensités correspondantes') #fonction input pour que l'utilisateur définisse les brones de l'intervalle
-float(intervallefin)=input('veuillez indiquer une fin d intervalle pour les longueurs d ondes pour lesquelles vous souhaitez avoir les intensités correspondantes')
+float(intervalledebut)=float(input('veuillez indiquer un début d intervalle pour les longueurs d onde pour lesquelles vous souhaitez avoir les intensités correspondantes')) #fonction input pour que l'utilisateur définisse les brones de l'intervalle
+float(intervallefin)=float(input('veuillez indiquer une fin d intervalle pour les longueurs d ondes pour lesquelles vous souhaitez avoir les intensités correspondantes'))
 
 print ('voici les intensités correspondantes à cet intervalle et la représentation graphique')
 
@@ -25,7 +25,7 @@ for i in range (0, len(longueurs_onde)):
     longueurs_ondesint.append(longueurs_onde[i])
     intensitesint.append(intensite[i])
 
-print (intensitesint); 
+print (intensitesint)
 
 
 plt.plot(longueurs_ondesint, intensitesint) #crée le plot, mais ne l'affiche pas
